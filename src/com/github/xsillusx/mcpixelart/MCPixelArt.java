5471e55ca9308af1e43bda07dff0a840ccaeb261
xSillusx <tkattanek00@gmail.com>
2018-11-29 23:46:39 +0100

    color conversion
    Added basic color conversion
    Just base rgb color values
    Like: If red is highest, place a red block and so on

--- src/com/github/xsillusx/mcpixelart/MCPixelArt.java
+++ src/com/github/xsillusx/mcpixelart/MCPixelArt.java
@@ -1,5 +1,6 @@
 package com.github.xsillusx.mcpixelart;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.BufferedInputStream;
@@ -12,8 +13,14 @@ import java.net.URL;
 
 import javax.imageio.ImageIO;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MCPixelArt extends JavaPlugin {
@@ -30,6 +37,7 @@ public class MCPixelArt extends JavaPlugin {
 	
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
 
+    	Player p = (Player)sender;
     	
     	// ==> https://pm1.narvii.com/6842/660e3758e9712b95e4a7e57fde3dee187d5abf92v2_hq.jpg <== TEST URL
     	if(cmd.getName().equalsIgnoreCase("pixelart")){ 
@@ -38,7 +46,7 @@ public class MCPixelArt extends JavaPlugin {
     		
     		
     		try {
-    			DownloadImage(args[0]);
+    			GeneratePixelart(args[0], p);
     		} catch (IOException e) {
     			this.getLogger().info(e.getMessage());
     		}
@@ -52,7 +60,7 @@ public class MCPixelArt extends JavaPlugin {
     	return false; 
 
     }
-	private void DownloadImage(String urlBase) throws IOException {
+	private void GeneratePixelart(String urlBase, Player playerRef) throws IOException {
 		URL url = new URL(urlBase);
 		InputStream in = new BufferedInputStream(url.openStream());
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
@@ -72,10 +80,59 @@ public class MCPixelArt extends JavaPlugin {
 		BufferedImage originalImage = ImageIO.read(new File("pictures/test.png"));
 		int type = originalImage.getType() == 0? BufferedImage.TYPE_INT_ARGB : originalImage.getType();
 
-		BufferedImage resizeImagePng = resizeImage(originalImage, type);
-		ImageIO.write(resizeImagePng, "png", new File("pictures/test.png")); 
+		//BufferedImage resizeImagePng = resizeImage(originalImage, type);
+		ImageIO.write(originalImage, "png", new File("pictures/test.png")); 
+		
+		World w = Bukkit.getWorld("world");
+		//String allPixels = "";
+		playerRef.playSound(playerRef.getLocation(), Sound.BLOCK_ANVIL_BREAK, 100f, 50f);
+		for(int y = 0; y < originalImage.getHeight(); y++) {
+			for(int x = 0; x < originalImage.getWidth(); x++) {
+				int px = originalImage.getRGB(x, y);
+				Color blockColor = new Color(px);
+				
+				// DEBUG ==> allPixels = allPixels + mycolor.getRed() + "," + mycolor.getGreen() + "," + mycolor.getBlue() + ";";
+				Location nextBlock = new Location(w, playerRef.getLocation().getX()+x, playerRef.getLocation().getY()-y, playerRef.getLocation().getZ());
+				
+				Material nextMat = GetMaterialByColor(blockColor);
+				
+				nextBlock.getBlock().setType(nextMat);
+			
+				
+			}
+		}
+		
+		//try (PrintWriter out1 = new PrintWriter("pictures/pixels.txt")) {
+		//    out1.println(allPixels);
+		//}
+		
 		
 	}
+	
+	private Material GetMaterialByColor(Color c) {
+		Material mat = Material.STONE;
+		int r = c.getRed();
+		int g = c.getGreen();
+		int b = c.getBlue();
+		
+		if(r > g && r > b) {
+			mat = Material.REDSTONE_BLOCK;
+		}
+		if(g > r && g > b) {
+			mat = Material.GREEN_WOOL;
+		}
+		if(b > g && b > r) {
+			mat = Material.LAPIS_BLOCK;
+		}
+		if(r == 255 && g == 255 && b == 255) {
+			mat = Material.SNOW_BLOCK;
+		}
+		if(r < 60 && g < 60 && b < 60) {
+			mat = Material.BLACK_WOOL;
+		}
+		return mat;
+	}
+	
 	
 	private static BufferedImage resizeImage(BufferedImage originalImage, int type){
 		BufferedImage resizedImage = new BufferedImage(50, 50, type);
