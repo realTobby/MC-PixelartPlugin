923f88e47269914ee23240750fc2a72c613785a1
xSillusx <tkattanek00@gmail.com>
2018-11-29 21:58:22 +0100

    Created command "pixelart"
    - downloads specified image from url into pictures folder

--- src/com/github/xsillusx/mcpixelart/MCPixelArt.java
+++ src/com/github/xsillusx/mcpixelart/MCPixelArt.java
@@ -1,5 +1,12 @@
 package com.github.xsillusx.mcpixelart;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -18,9 +25,21 @@ public class MCPixelArt extends JavaPlugin {
 	
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
 
-    	if(cmd.getName().equalsIgnoreCase("basic")){ 
+    	
+    	// ==> https://pm1.narvii.com/6842/660e3758e9712b95e4a7e57fde3dee187d5abf92v2_hq.jpg <== TEST URL
+    	if(cmd.getName().equalsIgnoreCase("pixelart")){ 
 
-    		sender.sendMessage("Command Works!");
+    		sender.sendMessage("Generating pixelart...");
+    		
+    		
+    		try {
+    			DownloadImage(args[0]);
+    		} catch (IOException e) {
+    			this.getLogger().info(e.getMessage());
+    		}
+    		
+    		
+    		
     		return true;
 
     	}
@@ -28,5 +47,22 @@ public class MCPixelArt extends JavaPlugin {
     	return false; 
 
     }
+	private void DownloadImage(String urlBase) throws IOException {
+		URL url = new URL(urlBase);
+		InputStream in = new BufferedInputStream(url.openStream());
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		byte[] buf = new byte[1024];
+		int n = 0;
+		while (-1!=(n=in.read(buf)))
+		{
+		   out.write(buf, 0, n);
+		}
+		out.close();
+		in.close();
+		byte[] response = out.toByteArray();
+		FileOutputStream fos = new FileOutputStream("pictures/test.png");
+		fos.write(response);
+		fos.close();
+	}
     
 }
